d52cd1724f33f34981de481c43bd884d98592b06
SYSUNetlab <64631790+SYSUNetlab@users.noreply.github.com>
2021-12-15 10:06:17 +0800

    -
    -

--- HMTDNN/Test_classify.m
+++ HMTDNN/Test_classify.m
@@ -1,11 +1,9 @@
-function [Confu_matrix, accuracy, P, R, pro_pre, label_pre] = Test_classify(X_test, label, net0, Class_number)
-                                                                                      
-
-        
-    probit=net0(X_test');   %输出观测值在每个状态的的预测概率,Q*T
+    function [Confu_matrix, accuracy, P, R, pro_pre, label_pre] = Test_classify(X_test, label, net0, Class_number) 
+    % TEST_CLASSIFY Ouputs the result of testing.
+    probit=net0(X_test');  
     [pro_pre,label_pre] = max(probit,[],1);
     index_t = find(label ==label_pre');
-    accuracy =  length(index_t)/length(label'); % 计算预测精度
+    accuracy =  length(index_t)/length(label'); 
     Confu_matrix = confusionmat(label',label_pre);
     
     for i= 1:Class_number
@@ -14,9 +12,7 @@ function [Confu_matrix, accuracy, P, R, pro_pre, label_pre] = Test_classify(X_te
         FP(i) = sum(Confu_matrix(:,i)) - Confu_matrix(i,i);
         TN(i) = sum(sum(Confu_matrix)) - TP(i) - FN(i) - FP(i);
     end
-    
     P = TP ./ (TP + FP);
     R = TP ./ (TP + FN);
-end
-    
+    end
     
